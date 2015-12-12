fd90d8ddfe51988ce05716296c803993cf58564e
Julian Hammer <julian.hammer@fau.de>
2015-12-12 17:18:05 +0100

    fixed gh-pages url

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-GIT_SSH_COMMAND="ssh -i deploy_key" git clone "https://${GH_REF}" -b gh-pages gh-pages
+GIT_SSH_COMMAND="ssh -i deploy_key" git clone "git@github.com:${GH_REF}" -b gh-pages gh-pages
 cp output/* gh-pages/
 cd gh-pages/
 tree -H . > index.html
