652190bdd051d2d37c198df41fbdd1311cfc332c
Julian Hammer <julian.hammer@fau.de>
2015-12-12 17:10:49 +0100

    added deployment key

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-git clone "https://${GH_REF}" -b gh-pages gh-pages
+GIT_SSH_COMMAND="ssh -i deploy_key" git clone "https://${GH_REF}" -b gh-pages gh-pages
 cp output/* gh-pages/
 cd gh-pages/
 tree -H . > index.html
 git config user.name "Travis CI"
 git add *
 git commit -m "Deployed from Travis CI"
-git push origin gh-pages
+GIT_SSH_COMMAND="ssh -i ../deploy_key" git push origin gh-pages
