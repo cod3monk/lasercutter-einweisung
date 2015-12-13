fa4937631d152769dc0f854bfd0c408f0c44bb0b
Julian Hammer <julian.hammer@fau.de>
2015-12-13 15:23:54 +0100

    debugging travis

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -2,7 +2,7 @@
 set -e
 set -v
 
-GIT_SSH_COMMAND="ssh -i deploy_key" git clone "git@github.com:${TRAVIS_REPO_SLUG}.git" -b gh-pages gh-pages
+GIT_SSH_COMMAND="ssh -v -i `pwd`/deploy_key" git clone "git@github.com:${TRAVIS_REPO_SLUG}.git" -b gh-pages gh-pages
 cp output/* gh-pages/
 cd gh-pages/
 tree -H . > index.html
@@ -12,3 +12,5 @@ git commit -m "Deployed from Travis CI"
 ls
 ls ..
 GIT_SSH_COMMAND="ssh -i ../deploy_key" git push origin gh-pages
+cd ..
+rm -rf gh-pages
